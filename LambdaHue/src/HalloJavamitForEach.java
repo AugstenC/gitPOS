8dbb1ccdc9866e21833596d75d31806382a2d436
AugstenC <AugstenC22036@sus.htl-grieskirchen.at>
2024-09-24 14:39:29 +0200

    made printList methode

--- LambdaHue/src/HalloJavamitForEach.java
+++ LambdaHue/src/HalloJavamitForEach.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class HalloJavamitForEach {
 
+    ArrayList<String> list = new ArrayList<String>();
+    public HalloJavamitForEach() {
+        list.add("Hallo");
+        list.add("Welt");
+        list.add("!");
+
+    }
+
+    public void printList() {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
 }
