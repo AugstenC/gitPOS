d7b9d00781840fab4e286cd727f7ec2975df3283
AugstenC <AugstenC22036@sus.htl-grieskirchen.at>
2024-09-24 15:29:29 +0200

    finished NumberTester

--- LambdaHue/src/HalloJavamitForEach.java
+++ LambdaHue/src/HalloJavamitForEach.java
@@ -3,16 +3,14 @@ import java.util.ArrayList;
 public class HalloJavamitForEach {
 
     ArrayList<String> list = new ArrayList<String>();
-    public HalloJavamitForEach() {
-        list.add("Hallo");
-        list.add("Welt");
-        list.add("!");
 
-    }
 
     public void printList() {
         for (String s : list) {
             System.out.println(s);
         }
+
+        list.forEach(System.out::println);
     }
+
 }
--- LambdaHue/src/NumberTester.java
+++ LambdaHue/src/NumberTester.java
@@ -1,2 +1,24 @@
 public class NumberTester {
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+    }
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+    
+
+    public void setPalindromeTester (NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+    public void testFile() {
+
+    }
+
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
 }
